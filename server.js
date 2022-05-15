7337268c0f544afa56f1f96245d139c0a507b705
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2022-05-16 01:36:16 +0530

    prepare to deploy on heroku

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 import express from 'express';
 import fetch from "node-fetch";
 import dotenv from 'dotenv';
+import * as path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 const app = express();
+
+
+// ... other app.use middleware
+app.use(express.static(path.resolve(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.get("/api/process-prompt/:prompt", (req, res) => {
     const {prompt} = req.params;
@@ -26,6 +41,7 @@ app.get("/api/process-prompt/:prompt", (req, res) => {
         },
         body: JSON.stringify(data),
     }).then(res => res.json()).then(_res=>{
+        console.log(_res.choices);
         res.send(_res.choices[0].text);
     });
 
